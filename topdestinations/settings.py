4e467b140a4e9d2541bcbd6a95a55fec4d87cca5
dcigic92 <dinocigic@yahoo.com>
2024-01-05 04:46:56 +0000

    Deployment commit

--- topdestinations/settings.py
+++ topdestinations/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n$o9r_m2&ul@y=x&_vq25+m9$aq=+7-7=j))r0gw=nh-3(j-i9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
