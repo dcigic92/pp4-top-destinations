d847fda65840e05cac9a94d8d472059d6ae56329
dcigic92 <dinocigic@yahoo.com>
2024-01-05 20:55:36 +0000

    Create post and country models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Countries"
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    destination_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    country_of_destination = models.ForeignKey(Country, on_delete=models.RESTRICT, null=True, related_name="country_of_destination")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="website_posts"
+        )
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.destination_name
