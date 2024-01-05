831ab46f3ce1885d57cea47227c3178960a4a7d1
dcigic92 <dinocigic@yahoo.com>
2024-01-05 05:24:28 +0000

    Change secret key

--- topdestinations/settings.py
+++ topdestinations/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n$o9r_m2&ul@y=x&_vq25+m9$aq=+7-7=j))r0gw=nh-3(j-i9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
