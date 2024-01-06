67ad38b7affe6e034098c6862aec6282bdca7b16
dcigic92 <dinocigic@yahoo.com>
2024-01-06 00:51:20 +0000

    Improve admin panel functionality

--- website/admin.py
+++ website/admin.py
@@ -4,6 +4,10 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content')
+    list_display = ('destination_name', 'country_of_destination', 'author', 'status', 'created_on')
+    search_fields = ['destination_name', 'content']
+    list_filter = ('status', 'created_on',)
+    prepopulated_fields = {'slug': ('destination_name',)}
+    summernote_fields = ('content',)
 
 admin.site.register(Country)
