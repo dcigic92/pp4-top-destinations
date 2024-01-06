175b2970b30e5a8b44f37199f55c355cb20cba42
dcigic92 <dinocigic@yahoo.com>
2024-01-06 20:39:01 +0000

    Connect base and index templates

--- website/views.py
+++ website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=0)
+    template_name = "website/index.html"
+    
