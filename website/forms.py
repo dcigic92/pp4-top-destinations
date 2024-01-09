48e2c691b34e24d5748e9bcbdfb3cb00c6a73a83
dcigic92 <dinocigic@yahoo.com>
2024-01-09 04:18:27 +0000

    Add user comments and fix styles

---
+++ website/forms.py
@@ -0,0 +1,12 @@
+from .models import Comment
+from django import forms
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['body'].label = ''
